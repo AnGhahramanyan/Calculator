d1504a1aa624d8d3087c8b54d69f10250f78e8c4
AnGhahramanyan <Ghahramanyan1997@gmail.com>
2020-07-18 17:10:22 +0400

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+namespace Calculator
+{
+    class Calculator
+    {
+        public static object Calculate(string formula)
+        {
+            var calc = new System.Data.DataTable();
+            return calc.Compute(formula, "");
+
+        }
+    }
+}
---
+++ Calculator/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Validator
+    {
+        public static bool Validate(string formula)
+        {
+            Console.WriteLine("Validating " + formula);
+
+            foreach (var s in formula)
+            {
+                if (!char.IsDigit(s) && (s != '+' && s != '-' && s != '/' && s != '*'))
+
+            
+                {
+                    
+                    return false;
+
+                }
+               
+            }
+
+            return true;
+
+
+        }
+
+    }
+
+
+
+}
